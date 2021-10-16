fa301e54c8980843f30545eda12b04e719fadfd9
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2021-10-17 00:04:37 +0700

    Add Percentage

--- analyze.js
+++ analyze.js
@@ -5,31 +5,31 @@ import * as fs from "fs/promises";
 let langObj = {};
 
 try {
-    const buffer = await fs.readFile("data.json");
-    langObj = JSON.parse(buffer.toString());
+  const buffer = await fs.readFile("data.json");
+  langObj = JSON.parse(buffer.toString());
 } catch (err) {
-    console.log("data.json not found");
-    process.exit(1);
+  console.log("data.json not found");
+  process.exit(1);
 }
 
 let langCounter = {};
 
 for (const repo in langObj) {
-    const langStats = langObj[repo];
-    for (const lang in langStats) {
-        if (langCounter[lang]) {
-            langCounter[lang] += parseInt(langStats[lang]);
-        } else {
-            langCounter[lang] = parseInt(langStats[lang]);
-        }
+  const langStats = langObj[repo];
+  for (const lang in langStats) {
+    if (langCounter[lang]) {
+      langCounter[lang] += parseInt(langStats[lang]);
+    } else {
+      langCounter[lang] = parseInt(langStats[lang]);
     }
+  }
 }
 
 const sorted = Object.entries(langCounter).sort(([, a], [, b]) => b - a);
 
 const languages = [];
 sorted.map((lang) => {
-    languages.push(lang[0]);
+  languages.push(lang[0]);
 });
 
 console.log(langCounter);
@@ -39,34 +39,39 @@ console.log(languages);
 let CSVWrite = "Repos,";
 
 for (const lang of languages) {
-    CSVWrite += `${lang},`;
+  CSVWrite += `${lang},`;
 }
 CSVWrite += "Total,\n";
 
 for (const reponame in langObj) {
-    let totalThisRepo = 0;
-    const repolang = langObj[reponame];
-    CSVWrite += `${reponame.split("/")[1]},`;
-    for (const lang of languages) {
-        if (repolang[lang]) {
-            CSVWrite += `${repolang[lang].toString()},`;
-            totalThisRepo += repolang[lang];
-        }
-        else {
-            CSVWrite += "0,";
-        }
+  let totalThisRepo = 0;
+  const repolang = langObj[reponame];
+  CSVWrite += `${reponame.split("/")[1]},`;
+  for (const lang of languages) {
+    if (repolang[lang]) {
+      CSVWrite += `${repolang[lang].toString()},`;
+      totalThisRepo += repolang[lang];
+    } else {
+      CSVWrite += "0,";
     }
-    CSVWrite += `${totalThisRepo},\n`;
+  }
+  CSVWrite += `${totalThisRepo},\n`;
 }
+
 CSVWrite += "Total,";
 
 let totalポイッ = 0;
 for (const lang of sorted) {
-    CSVWrite += `${lang[1]},`;
-    totalポイッ += lang[1];
+  CSVWrite += `${lang[1]},`;
+  totalポイッ += lang[1];
 }
 
 CSVWrite += `${totalポイッ},\n`;
+
+CSVWrite += "Percentage,";
+for (const lang of sorted)
+  CSVWrite += `${((100 * lang[1]) / totalポイッ).toFixed(2)},`;
+CSVWrite += "100.00,\n";
 
 await fs.writeFile("lang.csv", CSVWrite);
 
